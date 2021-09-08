e52548999a2084f258753d03d63273c3e1dc931e
Jesse Schalken <me@jesseschalken.com>
2021-09-09 01:11:12 +1000

    Fix missing dependencies during sync

--- java/src/com/google/idea/blaze/java/sync/importer/BlazeJavaWorkspaceImporter.java
+++ java/src/com/google/idea/blaze/java/sync/importer/BlazeJavaWorkspaceImporter.java
@@ -269,7 +269,7 @@ public final class BlazeJavaWorkspaceImporter {
     }
 
     // Add all deps if this target is in the current working set
-    if (workingSet == null || workingSet.isTargetInWorkingSet(target)) {
+    if (workingSet == null || workingSet.isTargetInWorkingSet(target) || true) {
       // Add self, so we pick up our own gen jars if in working set
       workspaceBuilder.directDeps.add(targetKey);
       for (Dependency dep : target.getDependencies()) {
